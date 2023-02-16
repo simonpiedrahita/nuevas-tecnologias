4cb6066eb48760dd55ca589eed3ded3c944914d9
simonpiedrahita <33105651+simonpiedrahita@users.noreply.github.com>
2023-02-16 08:23:20 -0500

    NT2: se modifica maain con f prints y sea crea archivo de condicionales

---
+++ condiciones.py
@@ -0,0 +1,1 @@
+#condicionales con python 
---
+++ main.py
@@ -0,0 +1,25 @@
+''' comentario de bloque 
+
+nombreUsuario="simon piedrahita"
+edadUsuario=33
+estaturaUsuario=1,76
+yaDesayuno= True '''
+
+# esto es un comentario 
+#datos primitivos 
+nombreUsuario="simon piedrahita"
+edadUsuario=33
+estaturaUsuario=1,76
+yaDesayuno= True
+
+#direccionUsuario=input("Digita tu direccion")
+
+#salida en consola 
+print("Hola tu nombre es "+ nombreUsuario+" Bienvenido ")
+print("Hpña tu edad es",edadUsuario," estamos viejos ")
+#print(f"tu direccion es {direccionUsuario} vive cerca ")
+
+var1= int(input("ingrese un numero"))
+var2= int(input("ingrese un numero"))
+sumatoria =var1+var2
+print(f"la sumatoria da por resultado {sumatoria}")
